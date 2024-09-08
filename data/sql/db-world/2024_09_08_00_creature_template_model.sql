f82b3109b497f3b571e2e6c03de002f077e95beb
Hallondricka <carlberg.marcus@gmail.com>
2024-09-08 07:44:50 +0200

    modelid1 change
    modelid1-4 is not being used anymore. Here is a fix for it.

---
+++ data/sql/db-world/2024_09_08_00_creature_template_model.sql
@@ -0,0 +1,6 @@
+INSERT INTO `creature_template_model`(`CreatureID`,`Idx`,`CreatureDisplayID`,`DisplayScale`,`Probability`,`VerifiedBuild`) values 
+(500030,0,25901,1,1,0);
+INSERT INTO `creature_template_model`(`CreatureID`,`Idx`,`CreatureDisplayID`,`DisplayScale`,`Probability`,`VerifiedBuild`) values 
+(500031,0,25901,1,1,0);
+INSERT INTO `creature_template_model`(`CreatureID`,`Idx`,`CreatureDisplayID`,`DisplayScale`,`Probability`,`VerifiedBuild`) values 
+(500032,0,18234,1,1,0);
